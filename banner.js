ecc7bf6679b801be6cce03801f07af6994333f61
Joshua <72642057+jeff450513@users.noreply.github.com>
2021-04-17 14:48:08 +0800

    Add files via upload

---
+++ banner.js
@@ -0,0 +1,15 @@
+const k = window.location.pathname
+const q = k.split('/')
+
+if (q[1] === 'index.html') {
+  $('.carousel1').addClass("owl-carousel owl-theme")
+  $('.owl-carousel').owlCarousel(OwlCarousel)
+} else {
+  $('.carousel1').removeClass("owl-carousel owl-theme")}
+</script>
+<script>
+  if (q[1] === 'about-us.html') {
+  $('.carousel').addClass("owl-carousel owl-theme")
+  $('.owl-carousel').owlCarousel(OwlCarousel)
+} else {
+  $('.carousel').removeClass("owl-carousel owl-theme")}
